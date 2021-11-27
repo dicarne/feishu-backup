7a424f67673250ca15d8bb827e4228bb56b440c9
dicarne <hzdingjiahe@outlook.com>
2021-11-27 21:42:20 +0800

    支持消息提示

--- feihu-backup-web/src/App.vue
+++ feihu-backup-web/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+import { NMessageProvider } from 'naive-ui'
 </script>
 
 <template>
-  <router-view/>
+  <n-message-provider>
+    <router-view />
+  </n-message-provider>
 </template>
 
 <style>
